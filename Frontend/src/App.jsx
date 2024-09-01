a7f410b4ce3eb41ee0be4d37e97d5e41d52e04c3
James Sembukuttiarachchi <jamessanjayasem@gmail.com>
2024-09-01 11:28:40 +0530

    Revert "skin tone detection"
    This reverts commit 705c733391689d53439f6ca19dbf83dc3a10cf44.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import EnterUserData from "./components/EnterUserData.jsx";
 import UserSizeRecm from "./components/UserSizeRecm.jsx";
 import SizeRecomSystem from "./pages/SizeRecomSystem.jsx";
-import SkinToneDetection from "./components/SkinToneDetection.jsx";
 
 const App = () => {
   return (
@@ -12,7 +11,6 @@ const App = () => {
         <Routes>
           <Route path="/" element={<EnterUserData />} />
           <Route path="/size" element={<UserSizeRecm />} />
-          <Route path="/detectskintone" element={<SkinToneDetection />} />
         </Routes>
       </BrowserRouter>
     </div>
