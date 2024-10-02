0260df9a1deb7dac6f9016dc98642e784576685a
James Sembukuttiarachchi <jamessanjayasem@gmail.com>
2024-10-02 11:09:45 +0530

    security update

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -3,24 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKaTxama3ugogULkEOr9n4rX9ajwpBesY",
-  authDomain: "stylizeme-7a7ed.firebaseapp.com",
-  databaseURL:
-    "https://stylizeme-7a7ed-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "stylizeme-7a7ed",
-  storageBucket: "stylizeme-7a7ed.appspot.com",
-  messagingSenderId: "131981295501",
-  appId: "1:131981295501:web:ae46075269939b4b9e87c4",
-
-  /*
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABSE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  */
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
