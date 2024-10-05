cdb3136ccb2eca65bc2ba6bf30c306182c9ed261
RakinduM <rakindumarambe17@gmail.com>
2024-10-06 00:36:43 +0530

    Ready to Build

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,6 @@ import AddSizes from "./components/AddSizes.jsx";
 import Signup from "./components/Signup.jsx";
 import Login from "./components/Login.jsx";
 import AdminRoute from "./routes/AdminRoute.jsx";
-import dummy from "./components/dummy.jsx";
 import Brands from "./components/Brands.jsx";
 import SaveSizes from "./components/SaveSizes.jsx";
 import UserProfile from "./components/UserProfile.jsx";
@@ -15,13 +14,7 @@ import AddProduct from "./pages/AddProduct"; // Adjust the path as necessary
 import ManageProducts from "./pages/ManageProducts"; // Adjust the path as necessary
 import ProductDetails from "./components/ProductDetails";
 import ProductList from "./components/ProductList";
-import PoseDetection from "./components/PoseDetection";
-import ViewTshirt from "./pages/ViewTshirt";
-import ProductCard from "./components/Product";
 import ShoppingHome from "./pages/ShoppingHome";
-import FaceMeshAR from "./components/FaceMeshAR";
-import ARHoodie from "./components/ARHoodie";
-import ARCap from "./components/ARCap";
 import FeedbackForm from "./pages/Feedback";
 import Feedbackview from "./pages/Feedbackview";
 import Feedbackupdate from "./pages/Feedbackupdate";
@@ -38,16 +31,15 @@ const App = () => {
     <div>
       <BrowserRouter>
         <Routes>
-          {/*James*/}
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
 
           <Route path="/detectskintone" element={<SkinToneDetection />} />
           <Route path="/home" element={<ShoppingHome />} />
-          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/contact" element={<Contact />} />
           <Route path="/Feedhome" element={<FeedbackHome />} />
           <Route path="/Feedback" element={<FeedbackForm />} />
-          <Route path="/MyFeedback" element={<MyFeedback/>}/>
+          <Route path="/MyFeedback" element={<MyFeedback />} />
           <Route path="/Feedbackview/:feedbackId" element={<Feedbackview />} />
           <Route
             path="/Feedbackupdate/:feedbackId"
@@ -62,10 +54,10 @@ const App = () => {
           <Route path="/userprofile" element={<UserProfile />} />
           <Route path="/menu" element={<ProductList />} />
           <Route path="/product/:id" element={<ProductDetails />} />
-          <Route path="/saved" element={<SavedProducts/>}/>
+          <Route path="/saved" element={<SavedProducts />} />
 
           <Route path="/admin" element={<AdminRoute></AdminRoute>} />
-          <Route path="/users" element={<UserManagement/>}/>
+          <Route path="/users" element={<UserManagement />} />
           <Route
             path="/addsizes"
             element={
@@ -100,7 +92,6 @@ const App = () => {
             }
           />
 
-          {/*Amaya*/}
           <Route
             path="/add-product"
             element={
@@ -117,16 +108,6 @@ const App = () => {
               </AdminRoute>
             }
           />
-
-          {/*Rakindu*/}
-
-          {/* <Route path="/view" element={<ViewTshirt />} />
-
-          <Route path="/glass" element={<FaceMeshAR />} />
-          <Route path="/hood" element={<ARHoodie />} />
-          <Route path="/cap" element={<ARCap />} /> */}
-
-          {/*Hiruni*/}
         </Routes>
       </BrowserRouter>
     </div>
